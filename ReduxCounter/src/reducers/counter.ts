81080ddbb25b0ec9b99163b4bff7cd2bdd104bc6
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-01-29 23:56:35 +0900

    カウンター用のtsファイルを作成

---
+++ ReduxCounter/src/reducers/counter.ts
@@ -0,0 +1,26 @@
+/**
+ * カウンター用のコンポーネントファイル
+ */
+
+import { Action } from "redux";
+
+/**
+ * 初期状態を表す関数
+ */
+export function createInitialState() {
+      return {
+            current: 0,
+      };
+} 
+
+export type State = ReturnType<typeof createInitialState>;
+
+/**
+ * reduxのベース
+ */
+export default function reducer(state = createInitialState(), action: Action) {
+      switch (action.type) {
+            default:
+                  return state;
+      }
+}
